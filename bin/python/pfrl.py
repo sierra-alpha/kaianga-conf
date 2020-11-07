34e94f515d48fdc823f2725b3bf542d580ab4f84
Shaun Alexander <shaun@sierraalpha.co.nz>
2020-11-07 01:12:34 +0000

    Scripted auto-commit on change (2020-11-07 01:12:34) by gitwatch.sh

--- bin/python/pfrl.py
+++ bin/python/pfrl.py
@@ -1,6 +1,7 @@
-#! /usr/env python
+# Should implement this in lua instead
+# From https://stackoverflow.com/questions/40993488/convert-markdown-links-to-html-with-pandoc
 
 import panflute as pf
 
 def action (elem, doc):
-   
+   if is
